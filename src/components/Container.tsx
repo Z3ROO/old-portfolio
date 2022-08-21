02f45b976b763aab803a0813922ca41d64fc2350
zero <zero@sara.com>
2022-08-21 01:53:13 -0300

    feature: option of no padding for Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,16 +1,16 @@
-export default function Container({children, className, level}:{children?:any, className?: string, level?:number}) {
+export default function Container({children, className, level, noPad}:{children?:any, className?: string, level?:number, noPad?:boolean}) {
 
   if (level == null)
     level = 0;
 
   const colorByLevel = [
-    'bg-gradient-to-br dark:from-gray-600 dark:to-gray-750 from-gray-100 to-gray-250 border dark:border-gray-700 border-gray-200',
-    'bg-gradient-to-br dark:from-gray-650 dark:to-gray-800 from-gray-200 to-gray-300 border dark:border-gray-750 border-gray-250',
-    'bg-gradient-to-br dark:from-gray-700 dark:to-gray-800 from-gray-250 to-gray-350 border dark:border-gray-800 border-gray-300'
-  ]
+    'bg-gradient-to-br dark:from-gray-600 dark:to-gray-750 from-gray-100 to-gray-250 outline outline-1 dark:outline-gray-700 outline-gray-200',
+    'bg-gradient-to-br dark:from-gray-650 dark:to-gray-800 from-gray-200 to-gray-300 outline outline-1 dark:outline-gray-750 outline-gray-250',
+    'bg-gradient-to-br dark:from-gray-700 dark:to-gray-800 from-gray-250 to-gray-350 outline outline-1 dark:outline-gray-800 outline-gray-300'
+  ];
 
   return (
-    <div className={`${colorByLevel[level]} p-6 rounded-lg shadow-lg dark:shadow-[#0006] ${className?className:''}`}>
+    <div className={`${colorByLevel[level]} ${noPad ? '' : 'p-6'} rounded-lg shadow-lg dark:shadow-[#0006] ${className?className:''}`}>
       {children}
     </div>
   )
