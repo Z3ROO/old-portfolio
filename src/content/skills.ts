29f10bb161af6f8c8de17d9edb7c0c77c51235ed
zero <zero@sara.com>
2022-08-21 12:41:26 -0300

    refactor: reorganize text content in seperate files

---
+++ src/content/skills.ts
@@ -0,0 +1,239 @@
+import { BurpLogo, OwaspLogo, WebProtectionIcon, HTMLLogo, CSSLogo, JsLogo, ReactLogo, ElectronJSLogo, PostgresLogo, MongoLogo, OSIcon, NetworkIcon, FundamentalsIcon, GraphQlLogo, NextJSLogo, GitLogo, RedisLogo, DockerLogo, JestLogo, GitLabLogo, CachingIcon, LinuxLogo, BashLogo, NPMLogo, WebpackLogo, BabelLogo, PostCSSLogo, TailwindCSSLogo, StyledComponents, ReactRouter, CrytoIcon, OAuthLogo, JWTLogo, VimLogo, PhotoshopLogo, IllustratorLogo, HackerIcon, DevelopmentIcon, DesignThinkingIcon } from "../lib/icons";
+
+
+const designThinkingPresentation = {
+  title: 'Design Thinking',
+  Icon: DesignThinkingIcon,
+  description: `Recently the idea of design thinking is strongly atetched to Product Design and its variants, but it doesnt stop there. It can be aplied to literally anything, from designing your day to designing your future depicting the appropriate decisions to achieve a certain goal, a certain result.
+  Of course that happens once you get a deep understanding of the fundamentals and from there it can be applied to anything.`
+}
+
+const securityPresentation = {
+  title: 'Security',
+  Icon: HackerIcon,
+  description: `A full year of intesive study of web hacking with takes on bugbounty hunting.A full year of intesive study of web hacking with takes on bugbounty hunting.`
+}
+
+const developmentPresentation = {
+  title: 'Development',
+  Icon: DevelopmentIcon,
+  description: `Around 10 years working with IT and Almost 5 of those practicing web development, currently on what some would refer to mid-to-senior level. I'm more confortable with front-end development but slowly shifting to backend with a more low-level programming approach.`
+}
+
+const designThinkingDetails = [
+  {
+
+  }
+]
+
+const securityDetails = [
+  {
+    title:'Burp',
+    Icon: BurpLogo,
+    description: 'burp burp burp burp burp burp burp burp burp burp burp burp burp burp '
+  },
+  {
+    title:'Owasp',
+    Icon: OwaspLogo,
+    description: 'owasp owasp owasp owasp owasp owasp owasp owasp owasp owasp owasp owasp owasp owasp owasp owasp '
+  },
+  {
+    title:'Browser Security',
+    Icon: WebProtectionIcon,
+    description: 'browser security browser security browser security browser security browser security browser security browser security browser security browser security browser security browser security browser security browser security browser security browser security '
+  }
+]
+
+const developmentDetails = [
+  {
+    title: 'HTML',
+    Icon: HTMLLogo,
+    descrition: [
+      'Conventions and best practices',
+      'Semantic structure',
+      'SEO',
+      'Basics of accessibility'
+    ]
+  },
+  {
+    title: 'CSS',
+    Icon: CSSLogo,
+    descrition: [
+      'Box Model, flexbox, grid.',
+      'Responsive design',
+      'Basics of SASS'
+    ]
+  },
+  {
+    title: 'Javascript',
+    Icon: JsLogo,
+    descrition: [
+      'ES6+',
+      'Promises, async/await',
+      'Core concepts: CallStack, EventLoop, hoisting, closure',
+      'DOM manipulations, fetch'
+    ]
+  },
+  {
+    title: 'ReactJS/Native',
+    Icon: ReactLogo,
+    descrition: []
+  },
+  {
+    title: 'ElectronJS',
+    Icon: ElectronJSLogo,
+    descrition: []
+  },
+  {
+    title: 'SQL',
+    Icon: PostgresLogo,
+    descrition: []
+  },
+  {
+    title: 'No-SQL',
+    Icon: MongoLogo,
+    descrition: []
+  },
+  {
+    title: 'O.S',
+    Icon: OSIcon,
+    descrition: []
+  },
+  {
+    title: 'Network fundamentals',
+    Icon: NetworkIcon,
+    descrition: []
+  },
+  {
+    title: 'Fundamentals',
+    Icon: FundamentalsIcon,
+    descrition: []
+  },
+  {
+    title: 'GraphQL',
+    Icon: GraphQlLogo,
+    descrition: []
+  },
+  {
+    title: 'NextJS',
+    Icon: NextJSLogo,
+    descrition: []
+  },
+  {
+    title: 'Git',
+    Icon: GitLogo,
+    descrition: []
+  },
+  {
+    title: 'Redis',
+    Icon: RedisLogo,
+    descrition: []
+  },
+  {
+    title: 'Docker',
+    Icon: DockerLogo,
+    descrition: []
+  },
+  {
+    title: 'Jest',
+    Icon: JestLogo,
+    descrition: []
+  },
+  {
+    title: 'GitLab',
+    Icon: GitLabLogo,
+    descrition: []
+  },
+  {
+    title: 'Caching',
+    Icon: CachingIcon,
+    descrition: []
+  },
+  {
+    title: 'Linux/WSL',
+    Icon: LinuxLogo,
+    descrition: []
+  },
+  {
+    title: 'Bash',
+    Icon: BashLogo,
+    descrition: []
+  },
+  {
+    title: 'npm',
+    Icon: NPMLogo,
+    descrition: []
+  },
+  {
+    title: 'Webpack',
+    Icon: WebpackLogo,
+    descrition: []
+  },
+  {
+    title: 'Babel',
+    Icon: BabelLogo,
+    descrition: []
+  },
+  {
+    title: 'PostCSS',
+    Icon: PostCSSLogo,
+    descrition: []
+  },
+  {
+    title: 'Tailwind',
+    Icon: TailwindCSSLogo,
+    descrition: []
+  },
+  {
+    title: 'Styled Components',
+    Icon: StyledComponents,
+    descrition: []
+  },
+  {
+    title: 'React Router',
+    Icon: ReactRouter,
+    descrition: []
+  },
+  {
+    title: 'Cryptography',
+    Icon: CrytoIcon,
+    descrition: []
+  },
+  {
+    title: 'OAuth',
+    Icon: OAuthLogo,
+    descrition: []
+  },
+  {
+    title: 'JWT',
+    Icon: JWTLogo,
+    descrition: []
+  },
+  {
+    title: 'Vim',
+    Icon: VimLogo,
+    descrition: []
+  },
+  {
+    title: 'Photoshop',
+    Icon: PhotoshopLogo,
+    descrition: []
+  },
+  {
+    title: 'Illustrator',
+    Icon: IllustratorLogo,
+    descrition: []
+  }
+];
+
+export const skillsPresentation = {
+  designThinking: designThinkingPresentation,
+  development: developmentPresentation,
+  security: securityPresentation,
+}
+
+export const skillsDetails = {
+  designThinking: designThinkingDetails,
+  development: developmentDetails,
+  security: securityDetails,
+}
