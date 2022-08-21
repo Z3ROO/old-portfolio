1f37dfe2903a0e59631f87cb49d49bbf4d40d69e
zero <zero@sara.com>
2022-08-21 01:58:02 -0300

    Initial functionality of skills details and refinement of skills icons apearance

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -12,6 +12,188 @@ const titleIcon = 'w-12 fill-red-400 inline-block';
 const burpDetails = `s,of web hacking with takee ap`;
 const owaspDetails = `asdasdass,adasof weasdasdb hacking wiasdasdasth takee ap`;
 
+const skills = [
+  {
+    title: 'HTML',
+    Icon: HTMLLogo,
+    descrition: [
+      'Conventions and best practices',
+      'Semantic structure',
+      'SEO',
+      'Basics of accessibility'
+    ]
+  },
+  {
+    title: 'CSS',
+    Icon: CSSLogo,
+    descrition: [
+      'Box Model, flexbox, grid.',
+      'Responsive design',
+      'Basics of SASS'
+    ]
+  },
+  {
+    title: 'Javascript',
+    Icon: JsLogo,
+    descrition: [
+      'ES6+',
+      'Promises, async/await',
+      'Core concepts: CallStack, EventLoop, hoisting, closure',
+      'DOM manipulations, fetch'
+    ]
+  },
+  {
+    title: 'ReactJS/Native',
+    Icon: ReactLogo,
+    descrition: []
+  },
+  {
+    title: 'ElectronJS',
+    Icon: ElectronJSLogo,
+    descrition: []
+  },
+  {
+    title: 'SQL',
+    Icon: PostgresLogo,
+    descrition: []
+  },
+  {
+    title: 'No-SQL',
+    Icon: MongoLogo,
+    descrition: []
+  },
+  {
+    title: 'O.S',
+    Icon: OSIcon,
+    descrition: []
+  },
+  {
+    title: 'Network fundamentals',
+    Icon: NetworkIcon,
+    descrition: []
+  },
+  {
+    title: 'Fundamentals',
+    Icon: FundamentalsIcon,
+    descrition: []
+  },
+  {
+    title: 'GraphQL',
+    Icon: GraphQlLogo,
+    descrition: []
+  },
+  {
+    title: 'NextJS',
+    Icon: NextJSLogo,
+    descrition: []
+  },
+  {
+    title: 'Git',
+    Icon: GitLogo,
+    descrition: []
+  },
+  {
+    title: 'Redis',
+    Icon: RedisLogo,
+    descrition: []
+  },
+  {
+    title: 'Docker',
+    Icon: DockerLogo,
+    descrition: []
+  },
+  {
+    title: 'Jest',
+    Icon: JestLogo,
+    descrition: []
+  },
+  {
+    title: 'GitLab',
+    Icon: GitLabLogo,
+    descrition: []
+  },
+  {
+    title: 'Caching',
+    Icon: CachingIcon,
+    descrition: []
+  },
+  {
+    title: 'Linux/WSL',
+    Icon: LinuxLogo,
+    descrition: []
+  },
+  {
+    title: 'Bash',
+    Icon: BashLogo,
+    descrition: []
+  },
+  {
+    title: 'npm',
+    Icon: NPMLogo,
+    descrition: []
+  },
+  {
+    title: 'Webpack',
+    Icon: WebpackLogo,
+    descrition: []
+  },
+  {
+    title: 'Babel',
+    Icon: BabelLogo,
+    descrition: []
+  },
+  {
+    title: 'PostCSS',
+    Icon: PostCSSLogo,
+    descrition: []
+  },
+  {
+    title: 'Tailwind',
+    Icon: TailwindCSSLogo,
+    descrition: []
+  },
+  {
+    title: 'Styled Components',
+    Icon: StyledComponents,
+    descrition: []
+  },
+  {
+    title: 'React Router',
+    Icon: ReactRouter,
+    descrition: []
+  },
+  {
+    title: 'Cryptography',
+    Icon: CrytoIcon,
+    descrition: []
+  },
+  {
+    title: 'OAuth',
+    Icon: OAuthLogo,
+    descrition: []
+  },
+  {
+    title: 'JWT',
+    Icon: JWTLogo,
+    descrition: []
+  },
+  {
+    title: 'Vim',
+    Icon: VimLogo,
+    descrition: []
+  },
+  {
+    title: 'Photoshop',
+    Icon: PhotoshopLogo,
+    descrition: []
+  },
+  {
+    title: 'Illustrator',
+    Icon: IllustratorLogo,
+    descrition: []
+  }
+];
+
 function ContainerTitle({title, Icon}:any) {
   return (
     <div className="mb-8">        
@@ -20,6 +202,7 @@ function ContainerTitle({title, Icon}:any) {
     </div>
   )
 }
+//6-9
 
 export default function Skills() {
   const [detailsContainer, setDetailsContainer] = useState<any>(null);
@@ -37,68 +220,7 @@ export default function Skills() {
           <p className="text-2xl p-2">Read <a className="cursor-pointer font-bold text-red-400">this post</a> to get a taste.</p>
           {/* #The necessity is the mother of all creations. */}
         </Container>
-        <Container className="row-span-2 flex flex-col xl:flex-col 2xl:flex-row">
-          <div className="flex-col">
-            <ContainerTitle title="Development" Icon={DevelopmentIcon} />
-            <p className="text-xl px-2 mb-4">
-              Around 10 years working with IT and Almost 5 of those practicing web development, currently on what some would refer to mid-to-senior level. I'm more confortable with front-end development but slowly shifting to backend with a more low-level programming approach.
-            </p>
-            <div className="pr-4">
-              <div className="flex flex-wrap justify-between">
-                <HTMLLogo className={bigIcon}/>
-                <CSSLogo className={bigIcon}/>
-                <JsLogo className={bigIcon}/>
-                <ReactLogo className={bigIcon}/>
-                <ElectronJSLogo className={bigIcon}/>    
-                <PostgresLogo className={bigIcon} />
-                <MongoLogo className={bigIcon} background="dark:fill-gray-700 fill-gray-200"/>
-                <OSIcon className={bigIcon}/>
-                <NetworkIcon className={bigIcon}/>
-                <FundamentalsIcon className={bigIcon}/>
-                <GraphQlLogo className={bigIcon} />      
-                <NextJSLogo className={bigIcon} />
-              </div>
-              <div className="flex flex-wrap justify-between">
-                <GitLogo className={mdIcon} />
-                <RedisLogo className={mdIcon} />
-                <DockerLogo className={mdIcon} background="dark:fill-gray-700 fill-gray-200"/>
-                <JestLogo className={mdIcon} background="dark:fill-gray-700 fill-gray-200" />
-                <GitLabLogo className={mdIcon} />
-                <CachingIcon className={mdIcon} />
-                <LinuxLogo className={mdIcon}/>
-                <BashLogo className={mdIcon}/>
-              </div>
-              <div>
-                <NPMLogo className={smallIcon}/>
-                <WebpackLogo className={smallIcon}/>
-                <BabelLogo className={smallIcon}/>
-                <PostCSSLogo className={smallIcon}/>
-                <TailwindCSSLogo className={smallIcon}/>
-                <StyledComponents className={smallIcon}/>
-                <ReactRouter className={smallIcon}/>
-                <CrytoIcon className={smallIcon} />
-                <OAuthLogo className={smallIcon} background="dark:fill-gray-700 fill-gray-200"/>
-                <JWTLogo className={smallIcon} />
-                <VimLogo className={smallIcon}/>
-                <PhotoshopLogo className={smallIcon}/>
-                <IllustratorLogo className={smallIcon}/>
-              </div>
-            </div>
-          </div>
-          {
-            false ? 
-            <div className="flex justify-center items-center dark:bg-gray-650 bg-gray-300 2xl:w-96 w-full rounded-lg shrink-0 2xl:h-full h-[calc(100%-26rem)] border border-2 border-dashed border-gray-400 shadow-inner shadow-[#0006]">
-              <span className="inline-block text-center text-2xl select-none opacity-60">
-                Click in one of the icons
-                <br />
-                <span className="text-7xl">&#11013;</span>
-              </span>
-            </div> :
-            <Container className="2xl:w-96 w-full shrink-0 2xl:h-full h-[calc(100%-26rem)]">
-
-            </Container>
-          }
-        </Container>   
+        <DevelopmentSection />
         <Container className={'relative'}>
           <ContainerTitle title="Security" Icon={HackerIcon} />
           <p className="text-xl">
@@ -122,6 +244,135 @@ export default function Skills() {
         </Container>
       </div>
     </section>
+  )
+}
+
+function DevelopmentSection() {
+  const [selectedSkill, setSelectedSkill] = useState<number|null>(null);
+  return (
+    <Container className="row-span-2 flex flex-col xl:flex-col 2xl:flex-row">
+      <div className="flex flex-col">
+        <ContainerTitle title="Development" Icon={DevelopmentIcon} />
+        <p className="text-xl px-2 mb-4">
+          Around 10 years working with IT and Almost 5 of those practicing web development, currently on what some would refer to mid-to-senior level. I'm more confortable with front-end development but slowly shifting to backend with a more low-level programming approach.
+        </p>
+        <SkillList selectedSkill={selectedSkill} setSelectedSkill={setSelectedSkill} />
+        {/* <div className="pr-4 relative flex flex-col justify-around flex-grow">
+          <div className="flex flex-wrap justify-between">
+            {
+              skills.map((skill, index) => {
+                if (index > 11)
+                  return
+                if (selectedSkill === index)
+                  return (
+                    <Container level={2} noPad>
+                      <skill.Icon className={bigIcon} onClick={() => setSelectedSkill(null)}/>
+                    </Container>
+                  )
+                return <skill.Icon className={bigIcon} onClick={() => setSelectedSkill(index)}/>
+              })
+            }
+          </div>
+          <div className="flex flex-wrap justify-between">
+            {
+              skills.map((skill, index) => {
+                if (index <= 11 || index > 19)
+                  return
+                if (selectedSkill === index)
+                  return (
+                    <Container level={2} noPad>
+                      <skill.Icon className={mdIcon} onClick={() => setSelectedSkill(null)}/>
+                    </Container>
+                  )
+                return <skill.Icon className={mdIcon} onClick={() => setSelectedSkill(index)}/>
+              })
+            }
+          </div>
+          <div className="flex flex-wrap justify-between">
+            {
+              skills.map((skill, index) => {
+                if (index <= 19)
+                  return
+                if (selectedSkill === index)
+                  return (
+                    <Container level={2} noPad>
+                      <skill.Icon className={smallIcon} onClick={() => setSelectedSkill(null)}/>
+                    </Container>
+                  )
+                return <skill.Icon className={smallIcon} onClick={() => setSelectedSkill(index)}/>
+              })
+            }
+            <div className="flex-grow"></div>
+          </div>
+        </div> */}
+      </div>
+      {
+        selectedSkill == null ? 
+        <div className="flex justify-center items-center dark:bg-gray-650 bg-gray-300 2xl:w-96 w-full rounded-lg shrink-0 2xl:h-full h-[calc(100%-26rem)] border border-2 border-dashed border-gray-400 shadow-inner shadow-[#0006]">
+          <span className="inline-block text-center text-2xl select-none opacity-60">
+            Click in one of the icons
+            <br />
+            <span className="text-7xl">&#11013;</span>
+          </span>
+        </div> :
+        <Container className="2xl:w-96 w-full shrink-0 2xl:h-full h-[calc(100%-26rem)]">
+          <h2 className="text-3xl font-bold mb-4">{skills[selectedSkill].title}</h2>
+          {
+            skills[selectedSkill].descrition.map((item) => (
+              <p className="m-2">&#8857; {item}.</p>
+            ))
+          }
+        </Container>
+      }
+    </Container>
+  )
+}
+
+function SkillList({selectedSkill, setSelectedSkill}:any) {
+  const structureConfig:[number, number, string][] = [
+    [0,5, "w-16 fill-red-400 m-3 inline-block opacity-75 hover:scale-110 transition-all hover:opacity-100"],
+    [5,11, "w-14 fill-red-400 m-3 inline-block opacity-75 hover:scale-110 transition-all hover:opacity-100"],
+    [12,20, "w-12 fill-red-400 m-2 inline-block opacity-75 hover:scale-110 transition-all hover:opacity-100"],
+    [20,30, "w-10 fill-red-400 m-2 inline-block opacity-75 hover:scale-110 transition-all hover:opacity-100"],
+    [30,42, "w-8 fill-red-400 m-2 inline-block opacity-75 hover:scale-110 transition-all hover:opacity-100"]
+  ]
+
+  return (
+    <div className="px-2 pr-6 pb-4 relative flex flex-col justify-around flex-grow">
+      {
+        Array(5).fill(null).map((val, row) => {
+          const [start, end, boxClass] = structureConfig[row]
+          return (
+            <div className={'grid items-center justify-center'} style={{gridTemplateColumns:`repeat(${end-start}, minmax(0, 1fr))`}}>
+              {
+                skills.map((skill, index) => {
+                  if (index >= start && index < end) {
+                    if (selectedSkill === index) {
+                      return (
+                        <button className="flex justify-center">
+                          <Container level={2} noPad>
+                            <skill.Icon className={boxClass} style={{opacity:1, transform:'scale(1.1)'}} onClick={() => setSelectedSkill(null)}/>
+                          </Container>
+                        </button>
+                      )
+                    }
+                    else
+                      return (
+                        <button className="flex justify-center">
+                          <skill.Icon className={boxClass} onClick={() => setSelectedSkill(index)}/>
+                        </button>
+                      )
+                  }
+                  else
+                    return null
+                })
+              }
+            </div>
+          )
+        }
+      )
+      }
+    </div>
   )
 }
 
