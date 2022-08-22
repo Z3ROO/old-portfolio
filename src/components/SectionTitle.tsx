cc5a0d5faf7cb97f3782c87bc163fc13b5a7cd1d
zero <zero@sara.com>
2022-08-22 18:04:09 -0300

    refactor: disabling selection of svg text

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -4,10 +4,10 @@ export default function SectionTitle({title}:any) {
   return (
     <div className="relative pt-24">
       <h1 className="text-6xl z-5 absolute bottom-2 left-6 inter-black">{title}</h1>
-      <div className="p-5 left-2 bottom-3 fade-stroke-text-light dark:fade-stroke-text-dark z-4 absolute">
+      <div className="p-5 left-2 bottom-3 fade-stroke-text-light dark:fade-stroke-text-dark z-4 absolute select-none">
         <div className="text-9xl inter-black text-transparent">{title}</div>
       </div>
-      <OutlineText className="stroke-red-400 text-9xl z-3 opacity-75" text={title} fontFamily="Inter Black" />
+      <OutlineText className="stroke-red-400 text-9xl z-3 opacity-75 pointer-events-none" text={title} fontFamily="Inter Black" />
     </div>
   )
 }
