fef4d72e0e1982e6ae681ece19530ee5f9c0649a
zero <zero@sara.com>
2022-08-22 18:05:31 -0300

    feature: Reorganize structure to dinamicly populate and adding icons

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,33 +1,51 @@
+import { projects } from "../content/projects";
 import Container from "./Container";
 import SectionTitle from "./SectionTitle";
 
 
-function Project(props: any) {
-  const { project, odd } = props;
+function Project({project}: any) {
 
   return  <Container className="flex my-6">
-            <div className="p-2 flex-col">
-              <div>
-                <h2 className="text-3xl">{project.title}</h2>
-                <p className="">{project.description}</p>   
+            <div className="p-2 flex flex-col flex-grow">
+              <div className="flex-grow">
+                <div>
+                  <h2 className="text-3xl font-bold mr-4 inline-block">{project.title}</h2>
+                  <div className="inline-flex self-end align-bottom">
+                    {project.screens.map((item: any) => (
+                      <button><item.Icon className="w-8 m-1 fill-red-400" /></button>
+                    ))}
+                  </div>
+                </div>
+                
+                <p className="mt-4">{project.description}</p>   
               </div>
-              <div className={`${odd && ''}`}>
-                <button className="btn">Detalhes do Projeto</button>
-                <br/>
-                <button className="btn">Github</button>
-                <button className="btn">Live demo</button>
+              <div className={`flex flex-col`}>
+                <div className={`flex m-2`}>
+                  {project.dependencies.map((item: any) => (
+                    <div title={item.title}><item.Icon className="w-4 -mb-1 m-1 fill-red-400"/></div>
+                  ))}
+                </div>
+                <div className={``}>
+                  <button className="btn">Detalhes do Projeto</button>
+                  
+                  <button className="btn">Github</button>
+                  <button className="btn">Live demo</button>
+                </div>
               </div>
             </div>
-            {/* <img src="" alt="Project gif presentation"/> */}
-            <div className={`w-[640px] h-[360px] bg-red-800 shrink-0 ${odd && '-order-1'}`}></div>
+            <div className={`w-[640px] h-[360px] bg-red-800 shrink-0 `}></div>
           </Container>
 }
+
+
 
 export default function Projects() {
   return  <>
             <SectionTitle title='Projects' />
-            <Project project={{title:'Titulo do projeto', description: 'Lorem Ipsum is simply dummychanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'}}/>
-            <Project project={{title:'Titulo do projeto', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'}} odd/>
-            <Project project={{title:'Titulo do projeto', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'}}/>
+            {
+              projects.map((proj) => (
+                <Project project={proj} />
+              ))
+            }
           </>
 }
