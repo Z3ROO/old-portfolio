c0e86cc1e19005c52ad1b4d4f9e559e1087a2670
zero <zero@sara.com>
2022-08-23 16:10:33 -0300

    feature: refine design of cards

--- src/components/MinorProjects.tsx
+++ src/components/MinorProjects.tsx
@@ -1,5 +1,5 @@
 import { minorProjects } from "../content/projects";
-import { GitLogo, NPMLogo } from "../lib/icons";
+import { GitLogo, NPMLogo, ReactLogo } from "../lib/icons";
 import Container from "./Container";
 import SectionTitle from "./SectionTitle";
 
@@ -20,17 +20,20 @@ export default function MinorProjects() {
 }
 
 function MinorProject({project}:any) {
+  let {title, description} = project;
+  if (title.length > 18)
+    title = title.substring(0,18)+'...'
   return (
     <Container className="w-60 mr-4">
-      <div className="w-48 h-48 bg-red-800"></div>
+      <ReactLogo className="w-48 h-48 fill-red-400"/>
       <div className="flex flex-col">
-        <div className="flex-grow">
-          <h2 className="font-bold text-lg">{project.title}</h2>
-          <p className="text-sm">{project.description}</p>
+        <div className="flex-grow h-[5.5rem] overflow-hidden mb-2">
+          <h2 className="font-bold text-lg">{title}</h2>
+          <p className="text-sm">{description}</p>
         </div>
         <div className="flex">
-          <GitLogo className="w-6 m-1" />
-          <NPMLogo className="w-6 m-1" />
+          <GitLogo className="w-6 m-1 fill-red-400" />
+          <NPMLogo className="w-6 m-1 fill-red-400" />
         </div>
       </div>
     </Container>
