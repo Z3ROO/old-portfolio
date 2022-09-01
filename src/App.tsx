503d63ccb7926183480f598c0e10bde88b3a334d
zero <zero@sara.com>
2022-09-01 17:14:23 -0300

    refactor: simple refactor App.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useRef, useState } from "react";
+import { createContext, useEffect, useRef } from "react";
 import GreetingSection from "./components/GreetingSection";
 import MinorProjects from "./components/MinorProjects";
 import Projects from "./components/Projects";
@@ -16,10 +16,8 @@ export const ControllerContext = createContext<any>(null);
 
 function App() {
   const controller = Controller();
-  const element = useRef<any>(null);
 
   useEffect(() => {
-    //document.querySelector('body')!.style.overflow = 'hidden'
     if (localStorage.getItem('colorMode') === 'dark') {
       document.querySelector('html')!.classList.add('dark');
     }
@@ -30,10 +28,10 @@ function App() {
       <ControllerContext.Provider value={controller} >
         <GreetingSection />
         <div className="flex">
-          <div className="w-1/12">
+          <div className="w-1/12 2xl:w-2/12">
 
           </div>
-          <div className="w-11/12 pr-16">
+          <div className="w-11/12 pr-16 max-w-[1440px] 2xl:10/12 2xl:pl-2 2xl:pr-14">
             <Projects />
             <MinorProjects />
             <Skills />
