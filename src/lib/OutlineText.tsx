903f336e8aa004e73cb743ae4c50403b93ce2e22
zero <zero@sara.com>
2022-09-02 18:13:53 -0300

    refactor: improving typescritp typing on OutlineText component

--- src/lib/OutlineText.tsx
+++ src/lib/OutlineText.tsx
@@ -1,6 +1,6 @@
-export default function OutlineText({text, className, fontFamily}: any) {
+export default function OutlineText({text, className, fontFamily}: {text:string, className:string, fontFamily:string}) {
   return (
-    <svg className={className} version="1.1" id="Camada_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="36px" width="max-content" height='min-content'>
+    <svg className={className} x="0px" y="36px" width="max-content" height='min-content'>
       <text transform="matrix(1 0 0 1 28.6582 101)" fill="none" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fontFamily={fontFamily}>{text}</text>
     </svg>
   )
